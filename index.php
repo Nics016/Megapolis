25fe566f3f4b8cf4d7d66f61e48a01d542c38144
Garraty <nics009@yandex.ru>
2016-10-27 14:31:58 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<title>Мегаполис</title>
+</head>
+<body>
+	<div id="main-wrap">
+		<header id="main-header">
+						
+		</header>
+	</div>
+</body>
+</html>
