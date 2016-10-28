27a24ecb227a91d1be75e3125a5c75d53dcf0bd1
Garraty <nics009@yandex.ru>
2016-10-29 01:44:54 +0300

    Added header.php

---
+++ header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<script src="js/main.js"></script>
+	<title>Мегаполис</title>
+</head>
+<body onload="bodyLoad()">
+	<div id="main-wrap">
+		<!-- HEADER -->
+		<header id="main-header">
+			<!-- CONTACT INFORMATION -->
+			<div class="container clearfix">
+				<div class="main-header-logo">
+					<a href="#" class="main-header-logo-pic"></a>
+				</div>
+				<div class="main-header-phone">
+					<a href="tel:8-495-215-1585" class="main-header-phone-text">
+						8 (495) 215-1585
+					</a>
+					<i class="fa fa-phone" aria-hidden="true"></i>
+					<span class="main-header-phone-subtext">
+						Общий многоканальный телефон
+					</span>
+				</div>
+				<div class="main-header-address">
+					<i class="fa fa-location-arrow" aria-hidden="true"></i>
+					<span class="main-header-address-text">
+						 г. Москва, ул. Шаболовка, <br>д. 34, стр. 3
+					</span>
+					<span class="main-header-address-subtext">
+						
+					</span>
+				</div>
+				<div class="main-header-cart clearfix">
+					<a href="#" class="main-header-cart-pic">
+					</a>
+					<a class="main-header-cart-inyourcart" href="#">
+						<span class="main-header-cart-inyourcart-text">
+							В Вашей корзине
+						</span>
+						<span class="main-header-cart-inyourcart-items">
+							товаров: <span class="main-header-cart-inyourcart-itemsNum" id="itemsNum">0</span> 
+						</span>
+						<span class="main-header-cart-inyourcart-items">
+							на сумму: <span class="main-header-cart-inyourcart-itemsNum" id="itemsSum">0 руб.</span> 
+						</span>
+					</a>
+				</div>
+			</div>
+			<!-- END OF CONTACT INFORMATION -->
+			<div id="top-nav" class="clearfix">
+				<ul id="top-nav-list">
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">Новости</a></li>
+					<li><a href="#">О компании</a></li>
+					<li><a href="#">Оплата и доставка</a></li>
+					<li><a href="#">Статьи</a></li>
+					<li><a href="#">Контакты</a></li>
+					<li><a href="#">Прайс</a></li>
+				</ul>
+			</div>
+		<hr class="shadowHr">
+		</header>
+		<!-- END OF HEADER -->
