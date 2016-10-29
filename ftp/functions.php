b0cca6459d04cc7fcf7cd0d7e8c7265aa47cb449
Nikita Abrashnev <Nikita Abrashnev>
2016-10-29 20:10:25 +0300

    All files are now stored in ftp folder

---
+++ ftp/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+add_filter('show_admin_bar', '__return_false'); //скроем на время админ бар сверху
+
+//подключаем стили
+function _s_styles()
+{
+	wp_register_style('main', get_template_directory_uri(). '/css/style.css');
+	wp_enqueue_style('main', get_template_directory_uri(). '/css/style.css');
+
+	wp_register_style('fontawesome', get_template_directory_uri(). '/font-awesome/css/font-awesome.min.css');
+	wp_enqueue_style('fontawesome', get_template_directory_uri(). '/font-awesome/css/font-awesome.min.css');
+}
+
+add_action('wp_enqueue_scripts', '_s_styles');
+
+//подключаю скрипты
+function _s_scripts()
+{
+	//своя библиотека jquery, не вордпрессовска
+	wp_deregister_script('jquery');
+	wp_register_script('jquery',  get_template_directory_uri(). '/js/jquery.js');
+	wp_enqueue_script('jquery');
+}
+
+add_action('wp_enqueue_scripts', '_s_scripts');
+
+//регистрация меню
+if ( function_exists( 'register_nav_menus' ) )
+{
+	register_nav_menus(
+		array(
+			'custom-menu'=>__('Custom menu'),
+		)
+	);
+}
+
+function custom_menu(){
+	echo '<ul>';
+	wp_list_pages('title_li=&');
+	echo '</ul>';
+}
---
+++ ftp/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<div id="main-wrap">
+	<!-- HEADER -->
+	<header id="main-header">
+		<!-- CONTACT INFORMATION -->
+		<div class="container clearfix">
+			<div class="main-header-logo">
+				<a href="<?= site_url() ?>" class="main-header-logo-pic"></a>
+			</div>
+			<div class="main-header-phone">
+				<a href="tel:8-495-215-1585" class="main-header-phone-text">
+					8 (495) 215-1585
+				</a>
+				<i class="fa fa-phone" aria-hidden="true"></i>
+				<span class="main-header-phone-subtext">
+					Общий многоканальный телефон
+				</span>
+			</div>
+			<div class="main-header-address">
+				<i class="fa fa-location-arrow" aria-hidden="true"></i>
+				<span class="main-header-address-text">
+					 г. Москва, ул. Шаболовка, <br>д. 34, стр. 3
+				</span>
+				<span class="main-header-address-subtext">
+					
+				</span>
+			</div>
+			<div class="main-header-cart clearfix">
+				<a href="#" class="main-header-cart-pic">
+				</a>
+				<a class="main-header-cart-inyourcart" href="#">
+					<span class="main-header-cart-inyourcart-text">
+						В Вашей корзине
+					</span>
+					<span class="main-header-cart-inyourcart-items">
+						товаров: <span class="main-header-cart-inyourcart-itemsNum" id="itemsNum">0</span> 
+					</span>
+					<span class="main-header-cart-inyourcart-items">
+						на сумму: <span class="main-header-cart-inyourcart-itemsNum" id="itemsSum">0 руб.</span> 
+					</span>
+				</a>
+			</div>
+		</div>
+		<!-- END OF CONTACT INFORMATION -->
+		<div id="top-nav" class="clearfix">
+			<!-- <ul id="top-nav-list">
+				<li><a href="#">Главная</a></li>
+				<li><a href="#">Новости</a></li>
+				<li><a href="#">О компании</a></li>
+				<li><a href="#">Оплата и доставка</a></li>
+				<li><a href="#">Статьи</a></li>
+				<li><a href="#">Контакты</a></li>
+				<li><a href="#">Прайс</a></li>
+			</ul> -->
+	        <?php
+		    		if ( function_exists( 'wp_nav_menu' ) )
+		        wp_nav_menu( 
+			        array( 
+			        'theme_location' => 'custom-menu',
+			        'fallback_cb'=> 'custom_menu',
+			        'container' => 'ul',
+			        'menu_id' => 'top-nav-list',
+			        'menu_class' => '') 
+						);
+		    		else custom_menu();
+					?>
+		</div>
+	<hr class="shadowHr">
+	</header>
+	<!-- END OF HEADER -->
